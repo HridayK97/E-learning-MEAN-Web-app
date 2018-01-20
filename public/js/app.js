cbd6c2eba42e20eb92c97dee7c4836dfe8d853cb
Hriday <hridayk@gmail.com>
2018-01-20 15:18:10 +0530

    Added UI Router

--- public/js/app.js
+++ public/js/app.js
@@ -1,1 +1,15 @@
-angular.module('sampleApp', ['ngRoute', 'appRoutes', 'MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService']);
+var myApp=angular.module('sampleApp', ['ui.router','MainCtrl', 'NerdCtrl', 'NerdService', 'GeekCtrl', 'GeekService','ngMaterial']);
+ 
+myApp.config(function($stateProvider, $urlRouterProvider){
+$stateProvider
+        .state('nerd', {
+            url: '/nerd',
+            templateUrl: 'views/nerd.html'
+        })
+        .state('geek', {
+            url: '/geek',
+            templateUrl: 'views/geek.html'
+        });
+ 
+    //$urlRouterProvider.otherwise('/nerd');
+});
